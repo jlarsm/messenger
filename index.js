18f72202dbcf7d46b4dba996769528a4e6f54cd2
jlarsm <jared.morris@ucalgary.ca>
2019-03-07 08:41:36 -0700

    Implemented rest of functionality
    still needs more testing and debugging

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var cookieParser = require('cookie-parser');
+var users = {};
+var colors = {};
+var messages = [];
+var usersocket ={};
 
-var users = [];
+app.use(cookieParser());
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -10,18 +15,44 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
-    var userId = socket.id;
-    var usernameId = socket.id.substring(0,9);
-    users[socket.id]= 'user-'+usernameId;
-    io.emit('chat message', 'Welcome user-' + usernameId);
-    io.emit('new username', users[socket.id]);
-    console.log(socket);
-    console.log(users);
+    console.log(socket.key);
+    var userId ='';
+    socket.on('connected', function(){
+        for(var i=0;i<messages.length;i++){
+            io.emit('update messages',messages[i][0],messages[i][1],messages[i][2],messages[i][3],messages[i][4],socket.id);
+        }
+    });
+    socket.on('get user', function(uid,username){
+        console.log(socket.id);
+        userId=uid;
+        if(uid in users){
+            console.log("yes its in here");
+            users[socket.id] = username;
+            colors[socket.id] = colors[uid];
+            usersocket[socket.id] = uid;
+        }
+        else{
+            console.log("nope not in here");
+            users[socket.id]= username;
+            colors[uid] = '#ffffff';
+        }
+        io.emit('server message', 'Welcome '+username);
+        io.emit('update userlist', JSON.stringify(users));
+    });
+    
+    
     socket.on('disconnect', function(){
         console.log('a user disconnected');
+        io.emit('server message', '[SERVER] '+users[socket.id]+' has disconnected');
+        users[socket.id] = '';
+        io.emit('update userlist', JSON.stringify(users));
+    });
+    socket.on('store message', function(msg){
+        messages.push(msg);
     });
     socket.on('chat message', function(msg){
         var currentdate = new Date();
+        console.log(userId);
         var time = currentdate.getHours() + ":" 
         + currentdate.getMinutes() + ":" 
         + currentdate.getSeconds() + ' ';
@@ -33,22 +64,42 @@ io.on('connection', function(socket){
             for(var i=0; i<keys.length; i++){
                 var key = keys[i];
                 if(users[key] === newUsername){
-                    io.emit('chat message', '[SERVER] '+newUsername
+                    io.emit('server message', '[SERVER] '+newUsername
                 + ' is already in use');
                     usernameTaken = 1;
                 }
             }
             if(!usernameTaken){
                 users[socket.id] = newUsername;
-                io.emit('chat message', '[SERVER] '+oldUsername
+                io.emit('server message', '[SERVER] '+oldUsername
                     + ' Changed their name to: '+
                     newUsername);
-                io.emit('new username', newUsername);
+                io.emit('change username',usersocket[socket.id],newUsername)
+                io.emit('update userlist', JSON.stringify(users));
             }
         }
-        console.log(msg);
-        var currentUser = users[socket.id];
-        io.emit('chat message', time+currentUser+": "+msg);
+        else if(msg.startsWith('/nickcolor ')){
+            var usernamecolor = msg.split('/nickcolor ');
+            var hexcheck = /([0-9A-F]{6}$)|([0-9A-F]{3}$)/i.test(usernamecolor);
+            if(hexcheck){
+                console.log(colors);
+                colors[socket.id] = '#'+usernamecolor[1];
+            }
+            else{
+                console.log("bad dog!")
+            }
+        }
+        else{
+            console.log(msg);
+            var currentuser = users[socket.id];
+            var socketid = socket.id;
+            var color = colors[socket.id];
+            console.log(color);
+            var array = [time,socketid,currentuser,msg,color];
+            console.log(array);
+            messages.push(array);
+            io.emit('chat message', time,socketid,currentuser,msg,color);
+        }
     });
 });
 
